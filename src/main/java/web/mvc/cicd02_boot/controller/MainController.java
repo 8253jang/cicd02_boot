f75cfbfd3b770c9ee20f4815e69857da00fd65ce
BongBong <8253jang@daum.net>
2025-05-13 16:41:50 +0900

    yml추가 push

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 Second CI/CD TEST";
+        return ".github>workflows/deploy.yml CI/CD TEST";
     }
 }
