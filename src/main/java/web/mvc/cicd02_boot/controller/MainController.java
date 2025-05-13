c9f0a9b69aca5a4df3f7cfe1a23aeb497c7132cf
BongBong <8253jang@daum.net>
2025-05-13 17:14:54 +0900

    수정함.

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return ".github>workflows/deploy.yml CI/CD TEST 완료 다시 수정 해서 push 되나요?";
+        return ".github>workflows/deploy.yml CI/CD TEST 완료 다시 수정 해서 push 하자요";
     }
 }
