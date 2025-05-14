77ad4782fde09f5afef01d9f48780140ea7ccd51
BongBong <8253jang@daum.net>
2025-05-14 15:03:53 +0900

    Docker 2 test

---
+++ src/test/java/web/mvc/cicd02_boot/Cicd02BootApplicationTests.java
@@ -0,0 +1,15 @@
+package web.mvc.cicd02_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd02BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test중입니다.");
+        //throw new RuntimeException("Fail!");
+    }
+
+}
